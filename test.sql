bd5209c2ef1aed90446841c36e420587e8374b8e
18Jaspal01 <86924458+18Jaspal01@users.noreply.github.com>
2022-01-06 10:32:33 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE studentData(Name varchar(20), Roll_No integer primary key NOT NULL, Address varchar(40), Mobile_Number int , PAN_Number varchar(10));
+
+INSERT INTO studentData values('Jaspal',20111015, 'Nashik',9834590641, 'PAN1234567');
+INSERT INTO studentData values('Adrash', 20111016, 'Jaipur',3214569870, 'CSD7654321');
+INSERT INTO studentData VALUES('Rohan', 20111017, 'Pune', 9632587410, 'ADC09876543');
