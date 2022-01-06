da42427be596e36a2c6935465c460d8aad0ae697
18Jaspal01 <86924458+18Jaspal01@users.noreply.github.com>
2022-01-06 21:44:06 +0530

    Add files via upload

--- test.sql
+++ test.sql
@@ -1,5 +1,54 @@
-CREATE TABLE studentData(Name varchar(20), Roll_No integer primary key NOT NULL, Address varchar(40), Mobile_Number int , PAN_Number varchar(10));
+-- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: pucsdStudents
+-- ------------------------------------------------------
+-- Server version	8.0.27
 
-INSERT INTO studentData values('Jaspal',20111015, 'Nashik',9834590641, 'PAN1234567');
-INSERT INTO studentData values('Adrash', 20111016, 'Jaipur',3214569870, 'CSD7654321');
-INSERT INTO studentData VALUES('Rohan', 20111017, 'Pune', 9632587410, 'ADC09876543');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `studentData`
+--
+
+DROP TABLE IF EXISTS `studentData`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `studentData` (
+  `Name` varchar(20) DEFAULT NULL,
+  `Roll_No` varchar(20) NOT NULL,
+  `Address` varchar(40) DEFAULT NULL,
+  `Mobile_Number` varchar(10) DEFAULT NULL,
+  `PAN_Number` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`Roll_No`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studentData`
+--
+
+LOCK TABLES `studentData` WRITE;
+/*!40000 ALTER TABLE `studentData` DISABLE KEYS */;
+INSERT INTO `studentData` VALUES ('Jaspal','20111015','Nashik','9834590641','PAN1234567'),('Adrash','20111016','Jaipur','3214569870','CSD7654321'),('Rohan','20111017','Pune','9632587410','ADC0987654');
+/*!40000 ALTER TABLE `studentData` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-06 15:59:30
